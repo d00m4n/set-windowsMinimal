c30e9ac06931987d73eedccb656a95abacad3e22
Dr_D00m4n <github@doomans.com>
2022-03-23 04:52:45 +0100

    added some new services and descriptions

--- set-windowsMinimal.ps1
+++ set-windowsMinimal.ps1
@@ -1,11 +1,13 @@
 #load services and desired status
 $services= Get-Content services.cfg
 foreach ($service in $services) {
-    if ($service.trim()[0] -eq "#") {
+    if ($service.trim()[0] -eq "#" -or ($null -eq $service.trim()[0]))  {
         continue
     }
     $serviceName,$serviceStatus = $service.split("=")
-    $set = Set-Service  $servicename -StartupType $serviceStatus
+    get-service "$($serviceName)*" | Set-Service  -StartupType $serviceStatus
+    # Exit
+    # $set = Set-Service  $servicename -StartupType $serviceStatus
     if ($(Get-Service $serviceName | Select-Object -ExpandProperty "starttype") -eq  $serviceStatus) {
         Write-Host "- Service $servicename $serviceStatus" -f cyan
     } else {
